ebbe98ac34cea41d5c812932762ca17e21f1b848
Joe Park-Kennaby <jparkkennaby@gmail.com>
2020-02-07 20:40:56 +0000

    fix clone-git-repository script - improve regex for moving all files including hidden files

--- includes/clone-git-repository.sh
+++ includes/clone-git-repository.sh
@@ -13,12 +13,19 @@ mkdir -p data/www/$project_dir_name/htdocs
 # cd data/www/$project_dir_name/htdocs
 
 # get project files from bitbucket repository
+# -C flag is for specifying a path
 git -C data/www/$project_dir_name/htdocs clone $project_repo_name repository-files
 
-# test command
-# mkdir -p test-project/repo-directory/project-name/htdocs
-# git -C test-project/repo-directory/project-name/htdocs clone git@bitbucket.org:supadu/wordpress-docker-installation-script.git repository-files
-
 # another flag or command might be required here to mv hidden files
-mv data/www/$project_dir_name/htdocs/repository-files/{.,}* ./
+# note: .[^.]* means all hidden files except . and ..
+mv data/www/$project_dir_name/htdocs/repository-files/{,.[^.]}* data/www/$project_dir_name/htdocs/
 rm -rf data/www/$project_dir_name/htdocs/repository-files/
+
+
+
+# testing command
+# mkdir -p test-project/repo-directory/
+# git -C test-project/repo-directory/ clone git@bitbucket.org:supadu/wordpress-docker-installation-script.git repository-files
+# mv test-project/repo-directory/repository-files/{,.[^.]}* test-project/repo-directory/
+# rm -rf test-project/
+
